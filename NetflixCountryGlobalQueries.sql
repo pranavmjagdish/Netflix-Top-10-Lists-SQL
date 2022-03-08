81e1c5eaf9be0b4972a4ab28fca9ce079bf882ac
Pranav <101136462+pranavmjagdish@users.noreply.github.com>
2022-03-08 09:57:42 +0100

    Update NetflixCountryGlobalQueries.sql

--- NetflixCountryGlobalQueries.sql
+++ NetflixCountryGlobalQueries.sql
@@ -4,7 +4,7 @@
 Select count(distinct(country_name)) as NumberOfUniqueCountries
 From CountryTop10
 
--- # Of Countries A T.V Show Was In The Top 10 List For 
+-- # Of Countries A T.V Show Was In The Top 10 List  
 
 Select ct.show_title,gt.category ,count(distinct(ct.country_name)) as UniqueCountries 
 From CountryTop10 ct
@@ -13,14 +13,14 @@ Where ct.category = 'TV' -- or Film
 Group by ct.show_title, gt.category
 Order by UniqueCountries desc 
 
--- Shows/Films In Top 10 List For Most Time 
+-- # of Weeks A T.V Show Was In The Top 10 List 
 
 Select show_title,count(show_title) as WeeksInTop10 , count(distinct(season_title)) as NumberOfSeasons
 From CountryTop10
 Group By show_title
 Order BY WeeksInTop10 DESC;
 
--- Shows/Films That Were Watched The Most 
+-- # of Hours Watched For A T.V Show 
 
 Select show_title, category, sum(weekly_hours_viewed) as GlobalWatchHours
 From GlobalTop10
